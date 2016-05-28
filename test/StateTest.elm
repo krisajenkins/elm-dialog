468fcd38c15d1e8e169546031cfbd18bcedace1a
Kris Jenkins <krisajenkins@gmail.com>
2016-05-28 19:56:57 +0100

    WIP Test

--- test/StateTest.elm
+++ test/StateTest.elm
@@ -1,9 +1,9 @@
 module StateTest exposing (tests)
 
 import ElmTest exposing (..)
-
+import Advanced.State
 
 tests : Test
 tests =
     ElmTest.suite "State"
-        []
+        [assert (Advanced.State.initialState == 5)
