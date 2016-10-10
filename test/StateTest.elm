dc4a6ea87ee7c4cd215ea508301e2320c8064dc8
Carsten König <Carsten@gettingsharper.de>
2016-10-10 08:18:52 +0200

    updated test suite for new elm-test

--- test/StateTest.elm
+++ test/StateTest.elm
@@ -5,22 +5,23 @@ import Advanced.Heroes.Types as Heroes exposing (View(BatmanView), Msg(BatmanMsg
 import Advanced.State exposing (initialModel, update)
 import Advanced.Types exposing (..)
 import Advanced.Villains.Types as Villains exposing (View(PenguinView))
-import ElmTest exposing (..)
+import Test exposing (..)
+import Expect exposing (greaterThan)
 
 
 tests : Test
 tests =
-    ElmTest.suite "State"
-        [ test "Attacks get transmitted."
-            (assert
+    describe "State"
+        [ test "Attacks get transmitted." <|
+            \() ->
                 (initialModel.villains.penguin.health
-                    > (List.foldl (\action model -> update action model |> fst)
-                        initialModel
-                        [ HeroesMsg (Heroes.SetView BatmanView)
-                        , VillainsMsg (Villains.SetView PenguinView)
-                        , HeroesMsg (BatmanMsg Kapow)
-                        ]
-                      ).villains.penguin.health
+                    `greaterThan`
+                        (List.foldl (\action model -> update action model |> fst)
+                            initialModel
+                            [ HeroesMsg (Heroes.SetView BatmanView)
+                            , VillainsMsg (Villains.SetView PenguinView)
+                            , HeroesMsg (BatmanMsg Kapow)
+                            ]
+                        ).villains.penguin.health
                 )
-            )
         ]
